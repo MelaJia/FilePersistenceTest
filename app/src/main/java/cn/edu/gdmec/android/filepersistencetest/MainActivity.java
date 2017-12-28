73095296aac89d7b534ac3ba38c5ed2e4cf6872a
MelaJia <838672863@qq.com>
2017-12-28 22:48:53 +0800

    文件存储

--- app/src/main/java/cn/edu/gdmec/android/filepersistencetest/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/filepersistencetest/MainActivity.java
@@ -2,27 +2,40 @@ package cn.edu.gdmec.android.filepersistencetest;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
 
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+/**
+ * 文件存储、java流读写操作
+ * 学习利用Context类中提供的openFileInput()和openFileOutput()方法
+ */
 public class MainActivity extends AppCompatActivity {
     private EditText edit;
-
-    public MainActivity() {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //加载布局
         edit= (EditText) findViewById(R.id.edit);
+        String inputText=load();
+        if (!TextUtils.isEmpty(inputText)){
+            edit.setText(inputText);
+            edit.setSelection(inputText.length());
+            Toast.makeText(this,"这是您上次的输入！",Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -53,4 +66,30 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    public String load(){
+        FileInputStream input = null;
+        BufferedReader reader = null;
+        StringBuilder content=new StringBuilder();
+        try {
+            input=openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(input));
+            String line="";
+            while ((line=reader.readLine())!=null){
+                content.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (reader!=null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        return content.toString();
+    }
+
 }
